c8a0899a6626dabb838175698baaeb0bcffac41a
Jacek Kosiba <jac.kosiba@gmail.com>
2017-07-28 14:06:27 +0200

    Pan knobs added

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ $(function(){ //DOMContentLoaded
 
   let tracksGroup = new Pizzicato.Group();
   let counter = 0;
-  const tracksArr = ['./../audio/BP/DRUMS.wav', './../audio/BP/BASS.wav', './../audio/BP/GTR.wav', './../audio/BP/VOC.wav'];
-  const tracksNames = ['DRUMS', 'BASS', 'GTR', 'VOC'];
-  const tracksSoloed = [false, false, false, false];
+  const tracksArr = ['./../audio/BP/DRUMS.wav', './../audio/BP/BASS.wav', './../audio/BP/GTR.wav', './../audio/BP/VOC.wav', './../audio/BP/DRUMS.wav', './../audio/BP/BASS.wav', './../audio/BP/GTR.wav', './../audio/BP/VOC.wav'];
+  const tracksNames = ['DRUMS', 'BASS', 'GTR', 'VOC', 'DRUMS2', 'BASS2', 'GTR2', 'VOC2'];
+  const tracksSoloed = [false, false, false, false, false, false, false, false];
 
 
 
@@ -30,7 +30,7 @@ $(function(){ //DOMContentLoaded
 
       // creating channel strips
 
-      let newStrip = $('<div class="strip"><div class="buttonBox"><button class="solo">S</button><button class="mute">M</button></div><div class="trackbox"><div class="track"><div data-id='+i+' class="fader'+i+' fader"></div></div></div><p class="label">'+tracksNames[i]+'</p></div>');
+      let newStrip = $('<div class="strip"><div class="effectBox"></div><div class="panBox"><div class="panKnob"><div class="knobMark"></div></div></div><div class="buttonBox"><button class="solo">S</button><button class="mute">M</button></div><div class="trackbox"><div class="track"><div data-id='+i+' class="fader'+i+' fader"></div></div></div><p class="label">'+tracksNames[i]+'</p></div>');
       $('.masterStrip').before(newStrip);
 
       track.volume = 0.6999;
@@ -65,6 +65,7 @@ $(function(){ //DOMContentLoaded
     $fader.on('mousemove', function(e) {
 
         e.preventDefault();
+        console.log(e.pageY);
 
         const $mute = $(this).parent().parent().prev().find('.mute');
         const $solo = $(this).parent().parent().prev().find('.solo');
@@ -77,7 +78,7 @@ $(function(){ //DOMContentLoaded
         } else {
 
             if( e.buttons === 1 ) {
-              $(this).css('top', e.pageY - 130);
+              $(this).css('top', e.pageY - 310);
               if( parseInt($(this).css('top')) >= 315 ) {
                 $(this).css('top', '315px');
               } else if( parseInt($(this).css('top')) < -35 ) {
@@ -324,8 +325,9 @@ $(function(){ //DOMContentLoaded
 
       e.preventDefault();
 
+
       if( e.buttons === 1 ) {
-        $masterFader.css('top', e.pageY - 130);
+        $masterFader.css('top', e.pageY - 310);
         if( parseInt($masterFader.css('top')) >= 315 ) {
           $masterFader.css('top', '315px');
         } else if( parseInt($masterFader.css('top')) < -35 ) {
@@ -346,19 +348,85 @@ $(function(){ //DOMContentLoaded
   });
 
 
-  //
+
+
+  // stereo pan feautures
 
 
 
+  const stereoPanner = new Pizzicato.Effects.StereoPanner({
+    pan: 0.0
+  });
+
+  tracksGroup.sounds.forEach( (t) => {
+    t.addEffect(stereoPanner);
+  });
+
+  function setPan(index, thisKnob, deg) {
+    // tracksGroup.sounds[index].effects[0].options.pan = ( parseInt(thisKnob.css('transform')) ;
+    if ( deg > 0 ) {
+      tracksGroup.sounds[index].effects[0].options.pan = (deg / 100) -0.4 ;
+    } else if ( deg < 0 ) {
+      tracksGroup.sounds[index].effects[0].options.pan = (deg / 100) +0.4 ;
+    } else {
+      tracksGroup.sounds[index].effects[0].options.pan = 0;
+    };
+
+  };
 
 
 
+  const panKnobs = document.querySelectorAll('.panKnob');
 
+  panKnobs.forEach( (t,i) => {
 
+    var dragging = false
 
+    $(function () {
+        var target = $('.panKnob').eq(i);
+        target.mousedown(function() {
+            dragging = true
+        })
+        $(document).mouseup(function() {
+            dragging = false
+        })
+        $(document).mousemove(function(e) {
+            if (dragging) {
 
+                var mouse_y = e.pageY;
+                var degree = mouse_y - 140
+                if( degree > 140 ) {
+                  degree = 140;
+                };
+                target.css('-moz-transform', 'rotate(' + degree + 'deg)');
+                target.css('-moz-transform-origin', '50% 50%');
+                target.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+                target.css('-webkit-transform-origin', '50% 50%');
+                target.css('-o-transform', 'rotate(' + degree + 'deg)');
+                target.css('-o-transform-origin', '50% 50%');
+                target.css('-ms-transform', 'rotate(' + degree + 'deg)');
+                target.css('-ms-transform-origin', '50% 50%');
 
+                setPan(i, target, degree);
 
+            };
+        });
+        target.on('dblclick', function() {
+
+            target.css('-moz-transform', 'rotate(' + 0 + 'deg)');
+            target.css('-moz-transform-origin', '50% 50%');
+            target.css('-webkit-transform', 'rotate(' + 0 + 'deg)');
+            target.css('-webkit-transform-origin', '50% 50%');
+            target.css('-o-transform', 'rotate(' + 0 + 'deg)');
+            target.css('-o-transform-origin', '50% 50%');
+            target.css('-ms-transform', 'rotate(' + 0 + 'deg)');
+            target.css('-ms-transform-origin', '50% 50%');
+
+            setPan(i, target, 0);
+
+        });
+    });
+  });
 
 
 
